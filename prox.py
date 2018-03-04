068827f630d71aee2dbb2c57ee315b8e8904161f
Ben <ba2490@columbia.edu>
2018-03-03 23:19:39 -0500

    server client communications

--- prox.py
+++ prox.py
@@ -11,15 +11,15 @@ def main():
 
     serv = socket.socket()
     serv.connect((host, cPort))
-    print("conneted to server - boom!")
-    serv.send('GET / HTTP/1.1')
+    print("conneted to server \n")
+
     s = socket.socket()
     s.bind((host, sPort))
     s.listen(5) 
 
     while True:
         c, addr = s.accept()
-        print("connection from: " + str(addr))
+        print("connection from: " + str(addr)+"\n")
         thread.start_new_thread(on_new_client, (serv, c, addr))
     c.close()
     serv.close()
@@ -27,10 +27,18 @@ def main():
 def on_new_client(serversocket, clientsocket, addr):
     while True: 
         msg = clientsocket.recv(1024)
+        
+        print("from client >> " + msg)
         if not msg:
             break
-        print("from "+str(addr) + " >> " + str(msg))  
         serversocket.send(msg)
+        msg = serversocket.recv(1024)
+        while msg:
+            print(msg)
+            clientsocket.send(msg)
+            msg = serversocket.recv(1024)
+        print("=====================================================================================================")
+
     print("closed socket with "+str(addr))
     clientsocket.close()
     
