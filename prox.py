fed1db71b05bf61dd11e87d1fd27450d92a84e99
Ben Arbib <ba2490@columbia.edu>
2018-03-06 16:53:05 -0500

    change header search

--- prox.py
+++ prox.py
@@ -46,10 +46,10 @@ def on_new_client(serversocket, clientsocket, addr):
         print("server >> " + msg)
         idx = msg.find("Content-Length:") + 16
         last = msg.find("\r\n", idx)
-        fileSize = int(msg[idx: last])
+        fileSize = int(msg[idx: last].strip)
         idx = msg.find("\r\n\r\n") + 4
         count = len(msg) - idx
-        print(str(count)+"-----------------------")
+        
         clientsocket.send(msg)
 
         diff = fileSize - count
