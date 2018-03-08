dbf31421dd1a3a4c5ff0393a479fc426ca0c35fa
Ben Arbib <ba2490@columbia.edu>
2018-03-08 03:18:59 -0500

    testing

--- prox.py
+++ prox.py
@@ -48,7 +48,7 @@ def on_new_client(serversocket, clientsocket, addr):
     while True:
         buff = 1024
         req = clientsocket.recv(buff) # GET
-        print(req)
+        print(req+"\n==================================================")
         if req.find(".f4m") != -1:
             if sendMan(req, serversocket, clientsocket, throughput) == -1:
                 print("closed in \"not\" SERVER clause "+str(addr))
@@ -109,7 +109,7 @@ def sendOther(req, serversocket, clientsocket, throughput):
 
     if not response:
         return -1
-
+    print(response+"\n=================================================================")
     idx = response.find("Content-Length:") + 16
     last = response.find("\r\n", idx)
     fileSize = int(response[idx: last].strip())
