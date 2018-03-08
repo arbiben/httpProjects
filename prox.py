4536e034659499f28efc944d6019125bb9302db9
Ben Arbib <ba2490@columbia.edu>
2018-03-07 19:18:16 -0500

    calculate throughput

--- prox.py
+++ prox.py
@@ -3,42 +3,50 @@ import socket
 import thread
 import select
 import time
+import xml.etree.ElementTree as xml
 
-def main():
-    #host = '127.0.0.1'
-    host = '3.0.0.1'
-    host2 = '4.0.0.1'
-    sPort = int(sys.argv[1])
-    cPort = 8080
-    # fakeIP = 
+if len(argv) != 3:
+    print("<alpha> <port>")
+    return
+#host = '127.0.0.1'
+host = '3.0.0.1'
+host2 = '4.0.0.1'
+sPort = int(sys.argv[2])
+cPort = 8080
+# fakeIP = 
+global alpha = float(sys.argv[1])
 
-    serv = socket.socket()
-    serv.connect((host, cPort))
-    print("conneted to 3.0.0.1 server ")
 
-    serv2 = socket.socket()
-    serv2.connect((host2, cPort))
-    print("conneted to 4.0.0.1 server \n")
+# <log > <alpha > <listen-port > <fake-ip > <web-server-ip >
+serv = socket.socket()
+serv.connect((host, cPort))
+print("conneted to 3.0.0.1 server ")
 
-    s = socket.socket()
-    s.bind((host, sPort))
-    s.listen(5)
+serv2 = socket.socket()
+serv2.connect((host2, cPort))
+print("conneted to 4.0.0.1 server \n")
 
-    while True:
-        c, addr = s.accept()
-        print("connection from: " + str(addr)+"\n")
-        thread.start_new_thread(on_new_client, (serv, c, addr))
-        # create a thread obj, 
-    c.close()
-    serv.close()
+s = socket.socket()
+s.bind((host, sPort))
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+    print("connection from: " + str(addr)+"\n")
+    thread.start_new_thread(on_new_client, (serv, c, addr))
+    # create a thread obj, 
+c.close()
+serv.close()
 
 def on_new_client(serversocket, clientsocket, addr):
+    throughput = 0
+    
     while True:
         buff = 1024
         msg = clientsocket.recv(buff) # GET
         
         if msg.find(".f4m") != -1:
-            msg = getMan(msg)
+            msg = getMan(msg, serversocket)
 
         # print("------------------- client -------------------\n" + msg)
         # print("------------------- client -------------------")
@@ -46,8 +54,11 @@ def on_new_client(serversocket, clientsocket, addr):
         serversocket.send(msg)        # send to server    
         msg = serversocket.recv(buff) # from server
         ttl = time.time()-t_start
-        throughput = getThroughput(ttl, len(msg))
+
+        throughput = getThroughput(ttl, len(msg), throughput)
+
         print(throughput)
+        
         if not msg:
             print("closed in \"not\" SERVER clause "+str(addr))
             clientsocket.close()
@@ -79,16 +90,18 @@ def on_new_client(serversocket, clientsocket, addr):
     print("closed socket with "+str(addr))
     clientsocket.close()
 
-def getMan(msg):
+
+def getMan(msg, serversocket):
+    serversocket.send(msg)
+    manif = serversocket.recv()
+
     parsed = msg.split(".f4m")
     msg = parsed[0] + "_nolist.f4m"+parsed[1]
     print(msg)
     return msg 
 
-def getThroughput(ttl, b):
-    return (1.0*b)/ttl
-
-if __name__=="__main__":
-    main()
+def getThroughput(ttl, b, t_curr):
+    t_new = b/ttl
+    return (alpha * t_new) + t_curr(1-alpha)
 
   #  /vod/big_buck_bunny.f4m
