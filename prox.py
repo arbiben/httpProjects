9efb84a6240211d386c44ba013d82b28b029e59a
Ben Arbib <ba2490@columbia.edu>
2018-03-08 14:40:59 -0500

    different structure

--- prox.py
+++ prox.py
@@ -1,93 +1,109 @@
 #!/usr/bin/env python
-import sys, socket ,thread, time, select
+import sys, socket ,thread, time, select, threading
 import xml.etree.ElementTree as xml
 
 # <log > <alpha > <listen-port > <fake-ip > <web-server-ip >
-if len(sys.argv) != 4:
-    print("<alpha> <port> <web-server>")
+if len(sys.argv) != 5:
+    print("<alpha> <port> <fake-ip> <web-server>")
     sys.exit()
 
 def main():
     global alpha
     global buffSize
+    global fake_ip
+    global client_port
+    global server_ip
+    global server_port
+    # global log
+
     buffSize = 1024
     # log = sys.argv[1]
     alpha = float(sys.argv[1])
     client_port = int(sys.argv[2])
-    # fakeIP = sys.argv
-    server_ip = sys.argv[3]
+    fake_ip = sys.argv[3]
+    server_ip = sys.argv[4]
     server_port = 8080
     
-    serv = socket.socket()
-    serv.connect((server_ip, server_port))
-    # serv2 = socket.socket()
-    # serv2.connect(('4.0.0.1', server_port))
-    print("conneted to server... \n")
+    # serv = socket.socket()
+    # serv.connect((server_ip, server_port))
+    # # serv2 = socket.socket()
+    # # serv2.connect(('4.0.0.1', server_port))
+    # print("conneted to server... \n")
 
     s = socket.socket()
-    s.bind((server_ip, client_port))
+    s.bind(('', client_port))
     s.listen(1)
     print("ready to connect")
 
+    
     while True:
         c, addr = s.accept()
         print("connection from: " + str(addr)+"\n")
-        thread.start_new_thread(on_new_client, (serv, c, addr))
+        #thread.start_new_thread(on_new_client, (serv, c, addr))
         
-        # args = (serv, c, addr)
-        # t = Thread(target=on_new_client, args=args)
-        # t.start()
-        # t.join()
+        args = (c)
+        t = Thread(target=on_new_client, args=args)
+        t.start()
+        t.join()
         # create a thread object 
-    c.close()
-    serv.close()
 
-def on_new_client(serversocket, clientsocket, addr):
+# openes new socket and creates a connection between client
+# proxy and server - reads a get request and resposes based on
+# the type of get request (mov, manifest, html...)
+def on_new_client(clientsocket):
+    serversocket = socket.socket()
+    serversocket.bind((fake_ip, 0))
+    serversocket.connect((server_ip, server_port))
+    print("connected to server")
+
     throughput = 10
     while True:
         buff = buffSize
         req = clientsocket.recv(buff) # GET
         
         if not req:
-            print("closed in \"not\" SERVER clause "+str(addr))
+            print("closed in \"not\" SERVER clause")
             clientsocket.close()
                 
         if req.find(".f4m") != -1:
             if sendMan(req, serversocket, clientsocket, throughput) == -1:
-                print("closed in \"not\" SERVER clause "+str(addr))
+                print("closed in \"not\" SERVER clause ")
                 clientsocket.close()
 
         else:
             if sendOther(req, serversocket, clientsocket, throughput) == -1:
-                print("closed in \"not\" SERVER clause "+str(addr))
+                print("closed in \"not\" SERVER clause ")
                 clientsocket.close()
 
-    print("closed socket with "+str(addr))
+    print("closed sockets")
     clientsocket.close()
+    serversocket.close()
+
 
 # get the manifest file
 def sendMan(req, serversocket, clientsocket, throughput):
     buff = buffSize
-    print("========================\n" + req + "\n=============================")
+    print("========begin=========\n" + req + "\n============end============")
     t_start = time.time()
     serversocket.send(req)
     response = serversocket.recv(buff)
     ttl = time.time()-t_start
 
-    manif = getManif(response, serversocket, clientsocket, throughput, False)
+    manif = getResponse(response, serversocket, clientsocket, throughput, False)
     
     parsed = req.split(".f4m")
-    req = parsed[0] + "_nolist.f4m"+parsed[1]
-    serversocket.send(req)
+    new_req = parsed[0] + "_nolist.f4m"+parsed[1]
+    serversocket.send(new_req)
     response = serversocket.recv(buff)
-    getManif(response, serversocket, clientsocket, throughput, True)
+    getResponse(response, serversocket, clientsocket, throughput, True)
     
 
-def getManif(response, serversocket, clientsocket, throughput, toClient):
-    
+# gathers the response in one file and if needed - saves it for use
+# otherwise, sende it to client
+def getResponse(response, serversocket, clientsocket, throughput, toClient):
     buff = buffSize
     fileSize, idx, count = getLength(response)
-    manif = response[idx:]
+    respose_file = response[idx:]
     
     if toClient:
         clientsocket.send(response)
@@ -98,8 +114,8 @@ def getManif(response, serversocket, clientsocket, throughput, toClient):
 
     while diff > 0:
         temp = serversocket.recv(buff)
-        manif += temp
-        count += len(response)
+        respose_file += temp
+        count += len(temp)
         if toClient:
             clientsocket.send(temp)
 
@@ -107,8 +123,12 @@ def getManif(response, serversocket, clientsocket, throughput, toClient):
         if diff < buff:
             buff = diff
     
-    return manif
+    if toClient:
+        return respose_file
 
+# gets the header of a response, parses it and returns
+# size of the file, index of where the body begins 
+# and count of the body that has been received in this chunk
 def getLength(response):
     
     idx = response.find("Content-Length:") + 16
@@ -119,6 +139,7 @@ def getLength(response):
     
     return [fileSize, idx, count]
 
+# if the response is not manifest it just sends it to the client
 def sendOther(req, serversocket, clientsocket, throughput):
     print("========================\n" + req + "\n=============================")
     buff = buffSize
@@ -132,8 +153,7 @@ def sendOther(req, serversocket, clientsocket, throughput):
     if not response:
         return -1
 
-    getManif(response, serversocket, clientsocket, throughput, True)
-    
+    getResponse(response, serversocket, clientsocket, throughput, True)
     return 0
 
 
