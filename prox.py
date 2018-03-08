dd6d0ee7ae6bfaff4e3428fb14acd8a6cc4bc472
Ben Arbib <ba2490@columbia.edu>
2018-03-08 01:08:57 -0500

    typo

--- prox.py
+++ prox.py
@@ -15,15 +15,14 @@ def main():
     global alpha
     alpha = float(sys.argv[1])
 
-
     # <log > <alpha > <listen-port > <fake-ip > <web-server-ip >
+
     serv = socket.socket()
     serv.connect((host, cPort))
-    print("conneted to 3.0.0.1 server ")
 
     serv2 = socket.socket()
     serv2.connect((host2, cPort))
-    print("conneted to 4.0.0.1 server \n")
+    print("conneted to servers... \n")
 
     s = socket.socket()
     s.bind((host, sPort))
@@ -33,30 +32,32 @@ def main():
         c, addr = s.accept()
         print("connection from: " + str(addr)+"\n")
         thread.start_new_thread(on_new_client, (serv, c, addr))
-        # create a thread obj, 
+        # create a thread object 
     c.close()
     serv.close()
 
 def on_new_client(serversocket, clientsocket, addr):
-    throughput = 0
+    throughput = 10
     
     while True:
         buff = 1024
         msg = clientsocket.recv(buff) # GET
         
         if msg.find(".f4m") != -1:
+            print(msg)
             msg = getMan(msg, serversocket)
+            print("=============================================")
+            print(msg)
 
-        print("------------------- client -------------------\n" + msg)
-        print("------------------- client -------------------")
+        # print("------------------- client -------------------\n" + msg)
+        # print("------------------- client -------------------")
         t_start = time.time()
         serversocket.send(msg)        # send to server    
         msg = serversocket.recv(buff) # from server
         ttl = time.time()-t_start
 
         throughput = getThroughput(ttl, len(msg), throughput)
-
-        print(throughput)
+        print("throughput is: " + str(throughput))
         
         if not msg:
             print("closed in \"not\" SERVER clause "+str(addr))
@@ -65,13 +66,12 @@ def on_new_client(serversocket, clientsocket, addr):
 
         idx = msg.find("Content-Length:") + 16
         last = msg.find("\r\n", idx)
-        print(str(idx)+"-----------------------------------------------" + str(last))
         fileSize = int(msg[idx: last].strip())
         idx = msg.find("\r\n\r\n") + 4
         count = len(msg) - idx
-        print(">>>>>>>>>>>>>>>>>>>>server>>>>>>>>>>>>>>>>>>>>>>> \n" + msg[:idx])
+        # print(">>>>>>>>>>>>>>>>>>>>server>>>>>>>>>>>>>>>>>>>>>>> \n" + msg[:idx])
         
-        print(">>>>>>>>>>>>>>>>>>>>server>>>>>>>>>>>>>>>>>>>>>>>")
+        # print(">>>>>>>>>>>>>>>>>>>>server>>>>>>>>>>>>>>>>>>>>>>>")
         
         clientsocket.send(msg)
 
@@ -98,7 +98,6 @@ def getMan(msg, serversocket):
 
     parsed = msg.split(".f4m")
     msg = parsed[0] + "_nolist.f4m"+parsed[1]
-    print(msg)
     return msg 
 
 def getThroughput(ttl, b, t_curr):
